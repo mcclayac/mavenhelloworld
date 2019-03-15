33a083f60e39c1a35f330050402ca454ab8b7050
mcclayac <anthony.mcclay@me.com>
2019-03-15 18:39:05 -0500

    Initial commit

---
+++ src/main/java/com/bigman/maven/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bigman.maven;
+
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World,  Tony McClay, was here");
+        System.out.println("and now I am not");
+        System.out.println("This is a simple application");
+
+    }
+}
